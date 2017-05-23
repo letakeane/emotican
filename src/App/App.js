d03f6cb5ea22962ab5e4f7a584874b0b483e7641
Leta Keane <letakeane+turing@gmail.com>
2017-05-23 12:27:08 -0600

    Connect components, install Router, style header

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { NavBar } from '../NavBar/NavBar';
+import { Instructions } from '../Instructions/Instructions';
+import { Play } from '../Play/Play';
+import { Learn } from '../Learn/Learn';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello!</div>
+      <Router>
+        <div>
+          <header>
+            <h1>emot<span className='logo-i'>i</span>can</h1>
+            <Route path='/' component={NavBar}/>
+          </header>
+            <Route exact path='/instructions' component={Instructions}/>
+            <Route exact path='/' component={Play}/>
+            <Route path='/learn' component={Learn} />
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/ImageCapture/ImageCapture.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ImageCapture = () => {
+  return (
+    <div>Image Capture goes here</div>
+  )
+}
