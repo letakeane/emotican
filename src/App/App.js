c94e8a68535801ae25c8facb3b1d4236d9b96a4d
Leta Keane <letakeane+turing@gmail.com>
2017-05-23 14:30:32 -0600

    Tweak styling for header/nav, install enzyme for testing

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { NavBar } from '../NavBar/NavBar';
 import { Instructions } from '../Instructions/Instructions';
 import { Play } from '../Play/Play';
 import { Learn } from '../Learn/Learn';
+import logoO from '../images/logo-o.png';
 import './App.css';
 
 class App extends Component {
@@ -12,7 +13,7 @@ class App extends Component {
       <Router>
         <div>
           <header>
-            <h1>emot<span className='logo-i'>i</span>can</h1>
+            <Link to='/'><h1>em<img className='logo-o' src={logoO} alt='&#9786;' />t<span className='logo-i'>i</span>can</h1></Link>
             <Route path='/' component={NavBar}/>
           </header>
             <Route exact path='/instructions' component={Instructions}/>
